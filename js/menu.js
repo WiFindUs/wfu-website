bca028140960296d83e9975465dd17d25b4887e4
hus0 <husseinalhammad@yahoo.com>
2015-01-09 02:13:04 +1030

    nav menu animation

--- js/menu.js
+++ js/menu.js
@@ -7,38 +7,87 @@ function toggleMenu()
 	if(!pulled)
 	{
 		nav.style.display = "block";
+		showMenu();
 		pulled = true;
 	}
 	
 	else
 	{
-		nav.style.display = "none";
+		hideMenu();
 		pulled = false;
 	}
 
 }
+
+
+function showMenu()
+{
+	var nav = document.getElementById("nav");
+	var left = -100;
+	var opacity = 0;
+	nav.style.opacity = opacity;
+	nav.style.left = left+"%";
+	
+	var timer;
+	timer = setInterval(function()
+	{
+		left = left+5;
+		nav.style.left = left+"%";
+		
+		if(opacity<1)
+		{
+			opacity = opacity+0.05;
+			nav.style.opacity = opacity;
+		}
+		
+		if(left==0)
+		{
+			window.clearInterval(timer);
+		}
+	}, 10);
+}
+
+function hideMenu()
+{
+	var nav = document.getElementById("nav");
+	var left = 0;
+	var opacity = 1;
+	nav.style.opacity = opacity;
+	nav.style.left = left+"%";
+	
+	var timer;
+	timer = setInterval(function()
+	{
+		left = left-5;
+		nav.style.left = left+"%";
+		
+		if(opacity>0)
+		{
+			opacity = opacity-0.07;
+			nav.style.opacity = opacity;
+		}
+		
+		if(left==-100)
+		{
+			window.clearInterval(timer);
+			nav.style.display = "none";
+		}
+	}, 10);
+}
+
 
 window.addEventListener('resize', clearStyle);
 
 function clearStyle()
 {
 	var nav = document.getElementById("nav");
-	
+	nav.style.opacity = 1;
 	if(window.innerWidth>800)
 	{
 		nav.style.display = "inline-block";
 	}
 	if(window.innerWidth<800)
 	{
-		/*if(nav.style.display == "none")
-		{
-			nav.style.display = "none";
-		}
-		else
-		{
-			nav.style.display = "block";
-		}*/
-		
 		nav.style.display = "none";
 		pulled = false;
 	}
