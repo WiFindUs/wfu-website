4c806ef5ef0afb4f1cc3b3a67b1ae66f4ed1f37e
hus0 <husseinalhammad@yahoo.com>
2015-02-02 23:10:06 +1030

    random stuff

---
+++ features.js
@@ -0,0 +1,17 @@
+
+window.addEventListener('resize', swapColumns);
+
+function swapColumns()
+{
+	var thumbnail = document.getElementById("featThumb");
+	var desc = document.getElementById("featDesc");
+	
+	var rectThumb = thumbnail.getBoundingClientRect();
+	var rectDesc = desc.getBoundingClientRect();
+	
+	thumbnail.style.top = rectDesc.top;
+	desc.style.top = rectThumb.top;
+	
+	console.log("desc:" +rectDesc.top);
+	console.log("thumb:" +rectThumb.top);
+}
