35686bb12582abb3de622df9cf2181bab80ad53e
hus0 <husseinalhammad@yahoo.com>
2015-02-11 05:27:10 +1030

    cross-browser centring elements

--- features.js
+++ features.js
@@ -1,18 +1,4 @@
-/*
-window.addEventListener('resize', swapColumns);
-
-function swapColumns()
+/*function fullscreen(source)
 {
-	var thumbnail = document.getElementById("featThumb");
-	var desc = document.getElementById("featDesc");
 	
-	var rectThumb = thumbnail.getBoundingClientRect();
-	var rectDesc = desc.getBoundingClientRect();
-	
-	thumbnail.style.top = rectDesc.top;
-	desc.style.top = rectThumb.top;
-	
-	console.log("desc:" +rectDesc.top);
-	console.log("thumb:" +rectThumb.top);
-}
-*/
+}*/
