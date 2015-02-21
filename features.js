eacfd40ba5c48cd9e850cf06634ad0e0e9cd4ece
hus0 <husseinalhammad@yahoo.com>
2015-02-21 18:30:44 +1030

    fuck CSS3 flexbox - using js instead
    CSS3 flexbox is not stable and there are different versions with
    different syntax (& may change in the future).
    
    Different browsers behave differently (e.g. IE has different default
    values to some attributes)
    
    So many variables to cater for. So using a js solution instead. If js
    not supported or disabled, tough shit

--- features.js
+++ features.js
@@ -1,4 +1,46 @@
-/*function fullscreen(source)
+var swapped = false;
+var boxNodeList;
+
+window.onload = function()
 {
+	getBoxes();
+	orderCheck();
+}
+
+function getBoxes()
+{
+	//NodeList objects
+	if(document.getElementsByClassName)
+	{
+		boxNodeList = document.getElementsByClassName("altFeat");
+	}
+	else //IE8
+	{
+		boxNodeList = document.querySelectorAll(".altFeat");
+	}
+}
+
+function switchOrder()
+{
+	for(var i=0; i<boxNodeList.length; i++)
+    {
+		boxNodeList[i].appendChild(boxNodeList[i].firstElementChild);
+	}
+}
+
+window.addEventListener('resize', orderCheck);
+
+function orderCheck()
+{
+	if(window.innerWidth < 900 && !swapped)
+	{
+		switchOrder();
+		swapped = true;
+	}
 	
-}*/
+	if(window.innerWidth >= 900 && swapped)
+	{
+		switchOrder();
+		swapped = false;
+	}
+}
